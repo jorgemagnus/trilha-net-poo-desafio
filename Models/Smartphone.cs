b5d02f70748c32097bc01ad040b462b3c01afd6c
jorgemagnus <jorgemagnus_rn@hotmail.com>
2022-11-05 10:59:23 -0300

    Desafio dio Smartphone finalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,24 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        //IMPLEMENTADO!
+        private  string Modelo { get; set; }
+        private  string IMEI { get; set; }
+        private  int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone()
+        {
+
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            // IMPLEMENTADO!
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -20,6 +33,17 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Recebendo ligação...");
         }
+
+        public virtual void SmartphoneMostrarConfiguracao(string nomeMarcaDispositivo)
+        {
+            //IMPLEMENTADO!
+            Console.WriteLine("Detalhe do Dispositivo: "+ nomeMarcaDispositivo);
+            Console.WriteLine("Numero: " +Numero);
+            Console.WriteLine("Modelo: " +Modelo);
+            Console.WriteLine("IMEI: " +IMEI);
+            Console.WriteLine("Memoria: " +Memoria);
+        }
+
 
         public abstract void InstalarAplicativo(string nomeApp);
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
+/*
+TODO: Realizar os testes com as classes Nokia e Iphone
+IMPLEMENTADO!
+*/
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Aparelho NOKIA");
+Smartphone nokia = new Nokia(numero: "0123456789", modelo: "XR20", imei: "0101012145145474", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.SmartphoneMostrarConfiguracao("NOKIA");
+
+Console.WriteLine("-------------------------------------------\n");
+
+Console.WriteLine("Aparelho IPHONE");
+Smartphone iphone = new Iphone(numero: "0987654321", modelo: "11 Pro", imei: "362457455211100", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.SmartphoneMostrarConfiguracao("IPHONE");
